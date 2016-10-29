8865fbc80d05316b32becad5c889334532670814
Jeff Kaufman <jeff@jefftk.com>
2016-10-29 13:28:28 +0000

    Completely rewrite query_padmapper.sh to work with the new padmapper api.  This version requires only one request to load all the data, which is great, but also requires some manual work.

--- query_padmapper.py
+++ query_padmapper.py
@@ -1,8 +1,10 @@
-import time
+import json
 import sys
 import urllib
-import json
-import time
+import urllib2
+import os.path
+
+YOUR_NAME = "YOUR NAME (your-email@example.com)"
 
 # boston
 MIN_LAT=42.255594
@@ -28,112 +30,53 @@ MAX_LON=-70.975800
 #MAX_LAT=37.83
 #MIN_LON=-122.70
 
-MAX_RENT=6050
+def download(fname):
+  base = os.path.basename(fname)
+  print "Visit:"
+  print 'https://www.padmapper.com/apartments/belmont-ma/belmont-hill?box=-71.1993028524,42.396054506,-71.1761285665,42.4262507215&property-categories=apartment'
+  print "Inspect the networking, find a pins request, copy request as curl and paste here."
+  inp = raw_input("> ")
+  if "--data-binary" not in inp:
+    raise Exception("Something looks wrong.  Was that the curl version of a pins request?")
 
-DEFAULTS = {
-    'cities': 'false',
-    'showPOI': 'false',
-    'limit': 2000,
-    'minRent': 0,
-    'maxRent': 6000,
-    'searchTerms': '',
-    'maxPricePerBedroom': 6000,
-    'minBR': 0,
-    'maxBR': 10,
-    'minBA': 1,
-    'maxAge': 7,
-    'imagesOnly': 'false',
-    'phoneReq': 'false',
-    'cats': 'false',
-    'dogs': 'false',
-    'noFee': 'false',
-    'showSubs': 'true',
-    'showNonSubs': 'true',
-    'showRooms': 'true',
-    'userId': -1,
-    'cl': 'true',
-    'apts': 'true',
-    'ood': 'true',
-    'zoom': 15,
-    'favsOnly': 'false',
-    'onlyHQ': 'true',
-    'showHidden': 'false',
-    'workplaceLat': 0,
-    'workplaceLong': 0,
-    'maxTime': 0
-    }
+  print "Run:"
+  print inp.split("--data-binary")[0] + '--data-binary \'{"bedrooms":[0,1,2,3,4,5],"limit":10000,"maxLat":%s,"minLat":%s,"maxLng":%s,"minLng":%s,"offset":0,"propertyCategories":["apartment"]}\' --compressed -o %s' % (
+    MAX_LAT, MIN_LAT, MAX_LON, MIN_LON, base)
+  raw_input("waiting...")
 
-def query(kwargs):
-    assert 'eastLong' in kwargs
-    assert 'northLat' in kwargs
-    assert 'westLong' in kwargs
-    assert 'southLat' in kwargs
+def process(fname_in, fname_out):
+  with open(fname_in) as inf:
+    data = json.loads(inf.read())
+  processed = []
+  for listing in data:
+    lat = listing["lat"]
+    lon = listing["lng"]
+    bedrooms = listing["min_bedrooms"]
+    rent = listing["min_price"]
+    apt_id = listing["listing_id"]
 
-    url='https://www.padmapper.com/reloadMarkersJSON.php'
+    processed.append((rent, bedrooms, apt_id, lon, lat))
 
-    full_url = '%s?%s' % (url, '&'.join('%s=%s' % (k,v) for (k,v) in kwargs.items()))
+  if len(processed) >= 9999:
+    print "should probably raise the limit in the pull"
 
-    apts = []
+  with open(fname_out, "w") as outf:
+    print "writing to %s" % fname_out
+    for rent, bedrooms, apt_id, lon, lat in processed:
+      outf.write("%s %s %s %s %s\n" % (rent, bedrooms, apt_id, lon, lat))
 
-    txt = ""
-    try:
-        txt = urllib.urlopen(full_url).read()
-        j = json.loads(txt)
-    except Exception, e:
-        print "ERROR", e
-        print "ERROR", txt
-        print "ERROR", full_url
-        return []
+def start(fname_download, fname_processed):
+  if not os.path.exists(fname_download):
+    download(fname_download)
+  if not os.path.exists(fname_download):
+    raise Exeption("%s still missing" % fname_download)
 
-    for apartment in j:
-        apts.append(( apartment['id'], apartment['lng'], apartment['lat'] ))
+  if not os.path.exists(fname_processed):
+    process(fname_download, fname_processed)
+  else:
+    print "%s already exists" % fname_processed
 
-    assert len(apts) < kwargs['limit']-1
+  print "Now you want to use draw_heatmap.py on %s" % fname_processed
 
-    return apts
-
-def start():
-    kwargs = dict((k,v) for (k,v) in DEFAULTS.items())
-    kwargs['southLat']=MIN_LAT
-    kwargs['westLong']=MIN_LON
-    kwargs['northLat']=MAX_LAT
-    kwargs['eastLong']=MAX_LON
-
-    seen_ids = set()
-
-    epoch_timestamp = int(time.mktime(time.gmtime()))
-    with open("apts-%s.txt" % epoch_timestamp, 'w') as outf:
-        for rent in range(100,MAX_RENT,25):
-            print "querying from $%s ..." % rent
-            for bedrooms in range(10):
-                kwargs['minRent'] = rent-25
-                kwargs['maxRent'] = rent
-                kwargs['minBR'] = bedrooms
-                kwargs['maxBR'] = bedrooms
-
-                for apt_id, lon, lat in query(kwargs):
-                    if apt_id not in seen_ids:
-                        outf.write("%s %s %s %s %s\n" % (
-                                rent, bedrooms, apt_id, lon, lat))
-                        sys.stdout.flush()
-                        seen_ids.add(apt_id)
-
-                time.sleep(2)
-
-
-if __name__=="__main__":
-    print """
-The guy who wrote Padmapper says this tool puts a pretty heavy load on his server and he
-would rather it was run no more than once a month.  If you're just looking for some
-apartment data, I've put some in apts-2013-01-29, which is for Boston in January 2013.
-"""
-    # start(*sys.argv[1:])
-
-    print """
-Ones labeled $6000 in the output file are really $6000+.  You can fix them manually
-by going to https://www.padmapper.com/show.php?type=0&id=[ID]&src=main for each one (the
-id is the third output column) and looking at what it says there.
-
-You probably also want to look over expensive '0 bedroom' apartments to check that none
-are commercial listings.
-"""
+if __name__ == "__main__":
+  start(*sys.argv[1:])
